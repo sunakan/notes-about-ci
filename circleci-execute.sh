8f3821af1cb2eb617509ee146d349d9f4ef50588
sunakan <sunakan03@gmail.com>
2019-03-30 16:54:11 +0900

    [Add] Workflowを使用したサンプル
    - 現在workflowを直接選択できない
      - よってjobを1つ1つ指定している

--- circleci-execute.sh
+++ circleci-execute.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
 circleci config process .circleci/config.yml > .circleci/config-2.0.yml
-circleci local execute -c .circleci/config-2.0.yml
+circleci local execute -c .circleci/config-2.0.yml --job alpha
+circleci local execute -c .circleci/config-2.0.yml --job beta
