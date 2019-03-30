c24d60d426e7fff995336bfeaeba553e11dc3372
sunakan <sunakan03@gmail.com>
2019-03-30 23:12:27 +0900

    [Modify] 環境変数の参照を微修正
    - うまく読み込めてなかったため

--- circleci-execute.sh
+++ circleci-execute.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 circleci config process .circleci/config.yml > .circleci/config-2.0.yml
-circleci local execute -c .circleci/config-2.0.yml -e DISCORD_WEBHOOK="${DISCORD_WEBHOOK}" --job feature
+circleci local execute -c .circleci/config-2.0.yml -e DISCORD_WEBHOOK=$DISCORD_WEBHOOK --job feature
 #circleci local execute -c .circleci/config-2.0.yml --job develop
 #circleci local execute -c .circleci/config-2.0.yml --job staging
 #circleci local execute -c .circleci/config-2.0.yml --job master
