c7e5fcbf17027466e26cd74bd32913c80b1e78b6
sunakan <sunakan03@gmail.com>
2019-04-04 21:33:48 +0900

    [Add] Issueの自動アプデのサンプルをsamaplesにcopy

--- circleci-execute.sh
+++ circleci-execute.sh
@@ -3,4 +3,4 @@ circleci config process .circleci/config.yml > .circleci/config-2.0.yml
 #circleci local execute -c .circleci/config-2.0.yml -e DISCORD_WEBHOOK=$DISCORD_WEBHOOK --job lint
 #circleci local execute -c .circleci/config-2.0.yml -e DISCORD_WEBHOOK=$DISCORD_WEBHOOK --job run_test_light
 #circleci local execute -c .circleci/config-2.0.yml -e DISCORD_WEBHOOK=$DISCORD_WEBHOOK --job merge_coverage_light
-circleci local execute -c .circleci/config-2.0.yml -e DISCORD_WEBHOOK=$DISCORD_WEBHOOK -e GITHUB_TOKEN=$GITHUB_TOKEN --job upseart_pullrequest
+#circleci local execute -c .circleci/config-2.0.yml -e DISCORD_WEBHOOK=$DISCORD_WEBHOOK -e GITHUB_TOKEN=$GITHUB_TOKEN --job upseart_
